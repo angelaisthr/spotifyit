84c1622f50817366c3aa3785ab280131adf26a59
angelaisthr <113161677+angelaisthr@users.noreply.github.com>
2023-01-12 04:03:55 +0000

    Added different method for access token, and reformatted some pages

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ app = Flask(__name__)
 SPOTIPY_CLIENT_ID = client_id = os.environ.get('client_id')
 SPOTIPY_CLIENT_SECRET = client_secret = os.environ.get('client_secret')
 SCOPE = "user-library-read playlist-read-private playlist-modify-public user-library-modify user-read-email user-read-private"
-SPOTIPY_REDIRECT_URI = "https://turnstrat.onrender.com/login"
+# SPOTIPY_REDIRECT_URI = "https://turnstrat.onrender.com/login"
+SPOTIPY_REDIRECT_URI = "https://angelaisthr-crispy-barnacle-rqv566r9wrx3pp6q-5000.preview.app.github.dev/login"
 
 sp_oauth = oauth2.SpotifyOAuth( SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET,SPOTIPY_REDIRECT_URI,scope=SCOPE)
 
@@ -34,18 +35,24 @@ def verify():
     Logs the user in
     """
     code = request.args.get('code')
-    print(code)
     if code:
         print ("Found Spotify auth code in Request URL! Trying to get valid access token...")
-        token_info = sp_oauth.get_access_token(code)
-        access_token = token_info['access_token']
+        try:
+            token_info = sp_oauth.get_cached_token()
+            print("\nCached", token_info)
+            print("\n\n")
+            access_token = token_info['access_token']
+        except:
+            token_info = sp_oauth.get_access_token(code)
+            print("NEW",token_info)
+            print("\n\n")
+            access_token = token_info['access_token']
     if access_token:
         print ("Access token available! Trying to get user information...")
         class b():
             global spotify_object
             spotify_object = spotipy.Spotify(access_token)
 
-    print(spotify_object)
     user_name = spotify_object.current_user()
     if user_name is None:
         return redirect("/")
@@ -71,6 +78,8 @@ def convert():
     """
     Gets liked songs and puts it into a playlist
     """
+    images = []
+    url = []
     user_name = spotify_object.current_user()
     user_id = user_name["id"]
     current_date =  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -80,7 +89,12 @@ def convert():
         if playlist['name'] == current_date:
             playlist_id = playlist['id']
             print(playlist['id'])
-            url = "https://open.spotify.com/playlist/" + playlist['id']
+            url.append("https://open.spotify.com/playlist/" + playlist['id'])
+            img = spotify_object.playlist_cover_image(playlist['id'])
+            print("IMAGE: ",img)
+            print("\n\n")
+            img = img[0]
+            images.append(img['url'])
             print("Loading...")
             results = spotify_object.current_user_saved_tracks()
             show_tracks(results, playlist_id)
@@ -89,9 +103,9 @@ def convert():
                 print("Please wait ...")
                 results = spotify_object.next(results)
                 show_tracks(results, playlist_id)
-    return render_template("works.html", playlistitems=playlists['items'], url=url)
+    return render_template("works.html", playlistitems=playlists['items'], urls=url, images=images)
 
-def show_tracks(results, playlist):
+def show_tracks(results, playlist_name):
     """
     Logic for getting liked songs
     """
@@ -105,11 +119,29 @@ def show_tracks(results, playlist):
         # track_uri = (track['uri'])
 
         # WORKS ONLY IF THE uri IS A LIST!!!!!!
-        spotify_object.playlist_add_items(playlist, [track['uri']])
+        spotify_object.playlist_add_items(playlist_name, [track['uri']])
         # ("%32.32s %s" % (track['artists'][0]['name'], track['name']))
-
 
 # FROM https://github.com/spotipy-dev/spotipy/blob/master/examples/show_my_saved_tracks.py
 
+@app.route("/playlists", methods=["GET", "POST"])
+def playlist():
+    stuff = []
+    user_name = spotify_object.current_user()
+    user_id = user_name["id"]
+    playlists = spotify_object.user_playlists(user_id)
+    for playlist in playlists['items']:
+        img = spotify_object.playlist_cover_image(playlist['id'])
+        if img == []:
+            if request.method == "POST":
+                img = "/static/no_image.gif"
+            else:
+                img = "/static/default.png"
+        else:
+            img = img[0]
+            img = img['url']
+        temp = {'url': "https://open.spotify.com/playlist/" + playlist['id'], 'name': playlist['name'], 'img': img}
+        stuff.append(temp)
+    return render_template("playlist.html", stuff=stuff)
 if __name__ == "__main__":
     app.run()
